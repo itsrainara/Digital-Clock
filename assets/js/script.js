147dfc91d453dd75fb50be596cc883e66f5d4eee
Rainara <89259827+itsrainara@users.noreply.github.com>
2024-10-09 11:51:43 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time() {
+    let dateToday = new Date();
+    let hrs = dateToday.getHours();
+    let mins = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if (hrs < 10) hrs = '0' + hrs;
+    if (mins < 10) mins = '0' + mins;
+    if (sec < 10) sec = '0' + sec;
+
+    hours.textContent = hrs;
+    minutes.textContent = mins;
+    seconds.textContent = sec;
+})
